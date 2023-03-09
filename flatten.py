c70e4f92e93ee78f7d2102f842fcb076f243efd1
Michal <mdyzma@gmail.com>
2023-03-09 11:47:45 +0100

    Added Docstring & CLI

--- flatten.py
+++ flatten.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import logging
 from typing import List
@@ -5,6 +6,17 @@ from typing import List
 logging.basicConfig(level=logging.INFO)
 
 def flatten(json_file_path: str, keys: List[str], output_file_path: str) -> bool:
+    """
+    Flattens a nested JSON file for a list of selected keys.
+
+    Args:
+        json_file_path (str): The path to the input JSON file.
+        keys (List[str]): The keys to flatten.
+        output_file_path (str): The path to the output file.
+
+    Returns:
+        bool: True if the operation was successful, False otherwise.
+    """
     try:
         with open(json_file_path) as f:
             data = json.load(f)
@@ -47,3 +59,21 @@ def flatten(json_file_path: str, keys: List[str], output_file_path: str) -> bool
     
     logging.info(f"Flattened JSON written to {output_file_path}")
     return True
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Flatten a JSON file.')
+    parser.add_argument('input', metavar='input', type=str,
+                        help='the path to the input JSON file')
+    parser.add_argument('keys', metavar='keys', type=str, nargs='+',
+                        help='the keys to flatten')
+    parser.add_argument('-o', dest='output', type=str, default='outputs.json',
+                        help='the path to the output file (default: output.json)')
+
+    args = parser.parse_args()
+
+    success = flatten(args.input, args.keys, args.output)
+
+    if success:
+        logging.info(f"Flattened JSON written to {args.output}")
+    else:
+        logging.error("Flattening failed.")
